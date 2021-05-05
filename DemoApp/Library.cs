8879c39d2d0b728882d970e40ebbb2a3bd8dd63e
Juan Carlos Becerra <Juan.Carlos.Becerra@ellucian.com>
2021-05-05 17:40:05 -0500

    Corrected issue on Add

--- DemoApp/Library.cs
+++ DemoApp/Library.cs
@@ -8,7 +8,7 @@ namespace DemoApp
     {
         public static int Add (int value1, int value2)
         {
-            return value1 * value2;
+            return value1 + value2;
         }
 
         public static int Multiply (int value1, int value2)
